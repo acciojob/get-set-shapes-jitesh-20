5a01a133f240ada8b201602824ec8a98a2af2f1f
Jitesh Kumar <105545377+jitesh-20@users.noreply.github.com>
2025-01-21 11:32:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
 //complete this code
 class Rectangle {
-	constructor(width,height){
-		if (width <= 0 || height <= 0) {
-	      throw new Error("Width and height must be positive integers.");
+  constructor(width, height) {
+    if (width <= 0 || height <= 0) {
+      throw new Error("Width and height must be positive integers.");
     }
-		this.width=width;
-		this.height=height;
-	}
-	get width(){
-		return this.width;
-	}
-	get height(){
-		return this.height;
-	}
-	getArea(){
-		return this.width*this.height;
-	}
+    this._width = width;
+    this._height = height;
+  }
+
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
 }
 
 class Square extends Rectangle {
-	constructor(side) {
+  constructor(side) {
     if (side <= 0) {
       throw new Error("Side must be a positive integer.");
     }
-    super(side, side);
+    super(side, side); 
   }
-	getPerimeter(){
-		return 4*this.side;
-	}
+
+  getPerimeter() {
+    return 4 * this._width; 
+  }
 }
+
+// Example usage
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width); 
+console.log(rectangle.height);
+console.log(rectangle.getArea()); 
+
+const square = new Square(7);
+console.log(square.width); 
+console.log(square.height); 
+console.log(square.getArea()); 
+console.log(square.getPerimeter()); 
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
